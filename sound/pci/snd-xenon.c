29d6852dea4aaae70225e24ad5854de096e32c0e
CaptureTheFrog <capturethefrog@proton.me>
2024-06-24 22:26:28 +0100

    Replace `snd_card_create()` usage with `snd_card_new()`

--- sound/pci/snd-xenon.c
+++ sound/pci/snd-xenon.c
@@ -625,7 +625,7 @@ static int snd_xenon_probe(struct pci_dev *pci,
 		return -ENOENT;
 	}
 
-	err = snd_card_create(index[dev], id[dev], THIS_MODULE, 0, &card);
+	err = snd_card_new(&pci->dev, index[dev], id[dev], THIS_MODULE, 0, &card);
 	if (err < 0)
 		return -ENOMEM;
 
